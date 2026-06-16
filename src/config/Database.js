47c0c6a4d32efa891e2a953deff5d2c514ee5d52
MIGUEL HENRI GOMES NUNES <miguel.nunes9@senaisp.edu.br>
2026-06-16 12:37:32 -0300

    atividade deploy

--- src/config/Database.js
+++ src/config/Database.js
@@ -46,7 +46,7 @@ export async function initializeDatabase() {
         });
 
 
-        const dbName = process.env.DB_DATABASE || 'desenvolvimento_sistema_produto';
+        const dbName = process.env.DB_DATABASE || 'deploy';
 
 
         await tempConnection.query(`CREATE DATABASE IF NOT EXISTS \`${dbName}\`;`);

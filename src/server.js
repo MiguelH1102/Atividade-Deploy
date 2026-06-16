458a8e4639efd4dd138ba0e44533fe21cc6c5e91
MIGUEL HENRI GOMES NUNES <miguel.nunes9@senaisp.edu.br>
2026-06-16 10:23:56 -0300

    atividade deploy

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import routes from './routes/routes.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { initializeDatabase } from './config/Database.js';
 
 // const cors = require('cors');
 
@@ -12,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Servidor rodando em: http://localhost:${process.env.SERVER_PORT}`);
+initializeDatabase().then(() => {
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+    });
+}).catch(err => {
+    console.error("Erro ao inicializar o banco de dados:", err);
 });
